6fbaa80e5e86871541ea51756fecd4cbcce359ed
fedcba499 <72005953+fedcba499@users.noreply.github.com>
2021-02-14 02:59:45 -0800

    gypsy changes

--- HC12_SENDER/HC12_SENDER.ino
+++ HC12_SENDER/HC12_SENDER.ino
@@ -1,20 +1,30 @@
 
 #include <SoftwareSerial.h>
 
+#include <LiquidCrystal.h>
+
 SoftwareSerial mySerial(2, 3); //RX, TX
+
+const int rs = 12, en = 11, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 int buttonPin = 8; 
 
 void setup() {
   pinMode(buttonPin, INPUT);
   mySerial.begin(9600);
+  lcd.begin(16,2);
+  lcd.setCursor( 1, 0);
+  lcd.print("201 ENGR REGT"); 
 }
 
 void loop() {
  
-  int buttonState = digitalRead(buttonPin);
+  int buttonState = digitalRead(buttonPin);   
   
-  if(buttonState == 1){//if button is down
+  if(buttonState == 1)
+  {//if button is down
     mySerial.println(1234);//send unique code to the receiver in this case 1234
   }
   delay(20);//delay little for better serial communication
---
+++ gypsy/gypsy.ino
@@ -0,0 +1,75 @@
+// First we inclue the libraries
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+//Data wire is pluggd into pin 2 on the Arduino
+#define ONE_WIRE_BUS 4
+
+int ledPin = 13;
+#define fuel 8
+#define engine 9
+
+
+
+float temp = 10.00;
+
+// Setup a OneWire instance to communicate with any OneWire Devices
+//(not just Maxim/Dallas temperature ICs)
+
+OneWire oneWire(ONE_WIRE_BUS);
+
+//Pass our oneWire reference to Dallas Temperature
+
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+    // Start up the library
+  sensors.begin();
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(fuel, OUTPUT);
+  pinMode(engine, OUTPUT);
+  digitalWrite(fuel, HIGH);
+  digitalWrite(engine, HIGH);
+}
+
+void loop(void) {
+
+  boolean ledState = digitalRead(ledPin);//check if the LED is turned on or off. Returns 1 or 0
+  
+  // put your main code here, to run repeatedly:
+  // call sensors.requestTemperatures() to issue a global temperature
+  // request to all devices on the bus
+
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures();
+  // Send the command to get temperature readings
+
+  Serial.println("DONE");
+
+  Serial.print("Temperature is:");
+  temp = sensors.getTempCByIndex(0);
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.print((char)176); //shows degrees character
+  Serial.print("C | ");
+
+  
+  // Why "byIndex"?
+  //You can have more than one DS18B20 on the same bus.
+  // 0 refers to the first IC on the wire
+
+  if(ledState == 0 && temp < 3){//if LED is off and button code is ok
+          digitalWrite(ledPin, HIGH);
+          digitalWrite(fuel , LOW);
+          delay(3000);       
+          digitalWrite(engine , LOW); 
+          delay(1000);
+          digitalWrite(engine , HIGH); 
+      }else if(ledState == 1 && temp > 20 ){//if LED is on and button code is ok
+          digitalWrite(ledPin, LOW);
+          digitalWrite(fuel , HIGH); 
+      }
+  delay(1000);
+
+}
